250419b649f03909145832a2a96efdd30cb9273c
wal33d006 <waleed_arshad006@hotmail.com>
2021-01-23 19:01:59 +0500

    Add restoration properties. Add search page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:state_restoration_bits/search_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    RootRestorationScope(
+      child: MyApp(),
+      restorationId: 'root',
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,109 +16,220 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: Colors.blueGrey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'State restoration'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with RestorationMixin {
+  final RestorableInt _scrollIndex = RestorableInt(0);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final ItemScrollController itemScrollController = ItemScrollController();
+  final ItemPositionsListener itemPositionsListener =
+      ItemPositionsListener.create();
+
+  final List<Email> users = [
+    Email(
+      title: 'Flutter Karachi',
+      day: 'Friday',
+      subject: 'Fluttering',
+      description: 'Let\'s get into code with Flutter',
+    ),
+    Email(
+      title: 'Google',
+      day: 'Monday',
+      subject: 'Security alert',
+      description: 'New device signed in to your google account',
+    ),
+    Email(
+      title: 'Waleed',
+      day: 'Saturday',
+      subject: 'State restoration',
+      description: 'Some tips and tricks regarding state restoration',
+    ),
+    Email(
+      title: 'Amazon',
+      day: 'Tuesday',
+      subject: 'Order update',
+      description: 'Your order has been confirmed',
+    ),
+    Email(
+      title: 'Daraz',
+      day: 'Wednesday',
+      subject: 'Refund successful',
+      description:
+          'You have been refunded your order against the tracking number 456635872354.',
+    ),
+    Email(
+      title: 'Netflix',
+      day: 'Saturday',
+      subject: 'Coming soon!',
+      description:
+          'Blacklist season 8, and 6 other shows are coming soon on Netflix',
+    ),
+    Email(
+      title: 'Adobe',
+      day: 'Sunday',
+      subject: 'Security profile changed',
+      description:
+          'Your security profile has been changed. A new phone number has been added to your profile.',
+    ),
+    Email(
+      title: 'Azure DevOps',
+      day: 'Monday',
+      subject: 'Build failed',
+      description:
+          'Your build named Flutter Karachi has failed. Click here to view the details.',
+    ),
+    Email(
+      title: 'GitHub',
+      day: 'Tuesday',
+      subject: 'New comment on issue',
+      description: 'Issue 2343 has a new comment on your repository.',
+    ),
+    Email(
+      title: 'TestFlight',
+      day: 'Friday',
+      subject: 'Flutter Pakistan (early access) 1.0.5',
+      description:
+          'Flutter Pakistan is available to test on your Test Flight application',
+    ),
+    Email(
+      title: 'App Center Team',
+      day: 'Saturday',
+      subject: 'Build successful',
+      description:
+          'A new version of Flutter Pakistan is available on the Play Store to test.',
+    ),
+  ];
+
+  @override
+  void initState() {
+    _scrollToIndex();
+    super.initState();
+  }
+
+  void _scrollToIndex() async {
+    await Future.delayed(Duration(milliseconds: 500));
+    itemScrollController.scrollTo(
+      index: _scrollIndex.value,
+      duration: Duration(milliseconds: 1000),
+      curve: Curves.easeInOut,
+    );
+    itemPositionsListener.itemPositions.addListener(() {
+      _scrollIndex.value =
+          itemPositionsListener.itemPositions.value.first.index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => SearchPage(),
+                  ),
+                );
+              }),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ScrollablePositionedList.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          var user = users[index];
+          String initials = '';
+          if (user.title.contains(' ')) {
+            initials =
+                user.title.split(' ')[0][0] + user.title.split(' ')[1][0];
+          } else {
+            initials = user.title[0];
+          }
+          return Column(
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  child: Text(
+                    initials.toUpperCase(),
+                  ),
+                ),
+                title: Text(user.title),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      user.subject,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      user.description,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+                trailing: Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(2)),
+                  child: Text(
+                    user.day,
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+//            isThreeLine: true,
+              ),
+              Divider(),
+            ],
+          );
+        },
+        itemScrollController: itemScrollController,
+        itemPositionsListener: itemPositionsListener,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  @override
+  // TODO: implement restorationId
+  String get restorationId => 'scroll_view';
+
+  @override
+  void restoreState(RestorationBucket oldBucket, bool initialRestore) {
+    registerForRestoration(_scrollIndex, 'scroll_index');
+  }
+}
+
+class Email {
+  final String title;
+  final String description;
+  final String subject;
+  final String day;
+
+  Email({
+    this.title,
+    this.day,
+    this.subject,
+    this.description,
+  });
 }
---
+++ lib/search_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> with RestorationMixin {
+  final RestorableTextEditingController _controller =
+      RestorableTextEditingController(text: '');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            controller: _controller.value,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement restorationId
+  String get restorationId => 'search_page';
+
+  @override
+  void restoreState(RestorationBucket oldBucket, bool initialRestore) {
+    registerForRestoration(_controller, 'controller');
+  }
+}
